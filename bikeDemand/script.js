9b4edadd8a0ed8db9ff14644c246fb86ae40d341
JiYounKang <kingeve@gmail.com>
2020-09-18 11:35:18 +0200

    Add files via upload

--- bikeDemand/script.js
+++ bikeDemand/script.js
@@ -1,24 +1,27 @@
-const urlParams = new URLSearchParams(window.location.search);
-const rentalBikes = document.querySelector("#rentalBikes");
-const slider = document.querySelector("#slider");
-const descript = document.querySelector("#description");
-
-const options = {
+var urlParams = new URLSearchParams(window.location.search);
+var rentalBikes = document.querySelector("#rentalBikes");
+var slider = document.querySelector("#slider");
+var descript = document.querySelector("#description");
+var content = document.querySelector("#content");
+var enough=document.querySelector("#enough");
+var stillEnough=document.querySelector("#stillEnough");
+var onlyFew=document.querySelector("#onlyFew");
+var options = {
   type: "regression",
   debug: true
 };
 
-const modelDetails = {
+var modelDetails = {
   model: "model/model.json",
   metadata: "model/model_meta.json",
   weights: "model/model.weights.bin"
 };
 
-const neuralNetwork = ml5.neuralNetwork(options);
+var neuralNetwork = ml5.neuralNetwork(options);
 
 neuralNetwork.load(modelDetails, predict);
 
-const inputs = {
+var inputs = {
   Hour: parseInt(urlParams.get('day')),
   TemperatureC: parseFloat(urlParams.get('tmp')),
   Humidity: parseInt(urlParams.get('hmd')),
@@ -31,13 +34,13 @@ const inputs = {
 console.log(inputs);
 
 function predict() {
-  console.log("predicting");
   neuralNetwork.predict(inputs, function (err, results) {
     if (err) {
       console.log(err);
       return;
     }
-    const targetActive = results[0].RentedBikeCounted.toFixed(0);
+    console.log(results[0]);
+    var targetActive = results[0].RentedBikeCounted.toFixed(0);
     // descript.innerHTML+=targetActive;
     descript.innerHTML+="<br><br>"+"Hour: " + inputs.Hour;
     descript.innerHTML+="<br><br>"+"Temperature: " + inputs.Temperature + "°C";
@@ -47,19 +50,19 @@ function predict() {
     descript.innerHTML+="<br><br>"+"Holiday: " + inputs.Holiday;
     descript.innerHTML+="<br><br>"+"Functioning Day: " + inputs.FunctioningDay;
 
-    
+    content.classList.add('hidden');
     if (targetActive < 1000) {
-      descript.innerHTML += "<br><br><b>"+"There are enough bikes for rent.</b>";
+      enough.classList.remove('hidden');
       descript.style.backgroundColor = "green";
-    } else if (targetActive>1000 && targetActive <2000){
-      descript.innerHTML += "<br><br><b>"+"You must hurry. The bike could rent out soon.</b>";
+    } else if (targetActive>1000 && targetActive <3000){
+      stillEnough.classList.remove('hidden');
       descript.style.backgroundColor = "yellow";
     } else {
-      descript.innerHTML += "<br><br><b>"+"Only few bikes are left. It is possible that you cannot rent one.<b>";
+      onlyFew.classList.remove('hidden');
       descript.style.backgroundColor = "red";
     }
     
-    console.log(results[0]);
+
     rentalBikes.innerHTML = results[0].value.toFixed(0);
     slider.style.height = results[0].value.toFixed(0) / 6 + "px";
     slider.innerHTML = results[0].value.toFixed(0);
